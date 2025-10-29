d99873bbc8bedb5a9308bb8f06b998221e506d63
Jake He <jake.he@nextgen.group>
2025-10-30 10:52:09 +1100

    persist

--- todolist.js
+++ todolist.js
@@ -36,56 +36,46 @@ function createTaskElement(task) {}
  * READ: Renders the current state of the tasks array to the DOM.
  */
 function renderTasks() {
-  // DOM Manipulation: Clear the existing list
-  taskList.innerHTML = "";
+  tasks.forEach(addTask);
 }
 
+function persistTasks() {
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
 /**
  * READ: Loads tasks from localStorage (Mock GET /todos).
  */
-function fetchAndRenderTasks() {}
+function fetchAndRenderTasks() {
+  // loaded tasks from local storage
+  tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+  // render/display tasks
+  renderTasks();
+}
 
 // --- Mock CRUD Operations ---
 
 /**
  * CREATE: Adds a new task to the array (Mock POST /todos).
  */
-function addTask() {
-  addTaskBtn.className = addTaskBtn.className.replace(
-    "btn-primary",
-    "btn-success"
-  );
-  setTimeout(() => {
-    addTaskBtn.className = addTaskBtn.className.replace(
-      "btn-success",
-      "btn-primary"
-    );
-  }, 700);
-
-  const taskText = taskInput.value.trim();
-
-  if (taskText === "") {
-    showMessage("Task cannot be empty.", "danger");
-    return;
-  }
-  taskInput.value = "";
+function addTask({ name, id }) {
   const taskElement = document.createElement("li");
   // add class card p-2 mb-2
   taskElement.className = "card p-2 mb-2 d-flex justify-content-between";
-  //    <li class="d-flex justify-content-between">
-  //   <div>112121</div>
-  //   <div>1/2/2</div>
-  //   <button class="btn btn-danger">delete</button>
-  // </li>
+  taskElement.setAttribute("id", id);
+
   const taskTextElement = document.createElement("div");
-  taskTextElement.textContent = taskText;
+  taskTextElement.textContent = name;
   const taskDateElement = document.createElement("div");
-  taskDateElement.textContent = new Date();
+  //<i class="fa fa-trash" aria-hidden="true"></i>
+  taskDateElement.textContent = dayjs().format("DD/MM/YYYY hh:mm a");
   const deleteButton = document.createElement("button");
   deleteButton.textContent = "Delete";
-  deleteButton.className = "btn btn-danger";
+  deleteButton.className = "btn btn-danger w-10";
   deleteButton.addEventListener("click", () => {
     taskElement.remove();
+    tasks = tasks.filter((task) => task.id !== id);
+    persistTasks();
   });
 
   taskElement.append(...[taskTextElement, taskDateElement, deleteButton]);
@@ -104,8 +94,39 @@ function deleteTask(id, taskName) {}
 
 // --- Event Listeners (DOM API) ---
 
-// Attach the main event listener to the 'Add Task' button
-addTaskBtn.addEventListener("click", addTask);
+function addTaskHandler() {
+  addTaskBtn.className = addTaskBtn.className.replace(
+    "btn-primary",
+    "btn-success"
+  );
+  setTimeout(() => {
+    addTaskBtn.className = addTaskBtn.className.replace(
+      "btn-success",
+      "btn-primary"
+    );
+  }, 700);
 
+  const taskText = taskInput.value.trim();
+  console.log({ taskText });
+  if (taskText === "") {
+    showMessage("Task cannot be empty.", "danger");
+    return;
+  }
+  const task = {
+    id: Math.floor(Math.random() * 1000),
+    name: taskText,
+    createAt: new Date(),
+    completed: false,
+  };
+  tasks.unshift(task);
+  persistTasks();
+
+  addTask(task);
+  taskInput.value = "";
+}
+
+// Attach the main event listener to the 'Add Task' button
+addTaskBtn.addEventListener("click", addTaskHandler);
+console.log("page load");
 // Initial data load
 fetchAndRenderTasks();
